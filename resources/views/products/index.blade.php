aeeb8a816267e784ec39769533e4e7c8aa4e7a00
Ishtiaq Ahmed <136582839+dev-ishtiaq@users.noreply.github.com>
2023-12-26 00:00:24 +0600

    tabele added

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -23,6 +23,9 @@
         </div>
         <h1>New Products</h1>
       </div>
+      <table class="table table-dark table-striped">
+        ...
+      </table>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </html>
